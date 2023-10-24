8c27ca0aef770889832b7af18f83d218b31248bf
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2023-10-24 18:21:19 +0700

    Create User.cs
    tao model User chứa các thuộc tính

---
+++ User.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bai_1.Models
+{
+    public class User
+    {
+        public string login { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string re_Password { get; set; }
+    }
+}
